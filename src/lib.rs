391d407e15968ce7e3cc1f82e5e87ce19f3ed90f
epi <43392618+epi052@users.noreply.github.com>
2022-12-01 16:19:06 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -15,19 +15,6 @@ pub fn parse_args() -> AoCArgParser {
     AoCArgParser::parse()
 }
 
-/// read input file from src/inputs based on current file's filename
-pub fn get_input(filename: &str) -> String {
-    let this_file = Path::new(filename);
-    let mut stem = this_file.file_stem().unwrap().to_str().unwrap().split('-');
-
-    stem.next().unwrap(); // discard 'day'
-    let current_day = stem.next().unwrap();
-
-    let input_path = format!("src/inputs/input-{}", current_day);
-
-    read_to_string(input_path).expect("couldn't read input file")
-}
-
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Solver {
     PartOne,
